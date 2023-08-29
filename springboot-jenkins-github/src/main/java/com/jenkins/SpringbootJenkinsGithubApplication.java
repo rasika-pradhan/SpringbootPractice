0f3f8272072c650283f06baebc3d5f5c2b028ac0
rasikapradhan1@gmail.com <rasikapradhan1@gmail.com>
2023-08-29 13:13:06 +0530

    Added a project

---
+++ springboot-jenkins-github/src/main/java/com/jenkins/SpringbootJenkinsGithubApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsGithubApplication.class, args);
+	}
+
+}
---
+++ springboot-jenkins-github/src/test/java/com/jenkins/SpringbootJenkinsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
